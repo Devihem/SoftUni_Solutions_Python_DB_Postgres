660f5a8a36bab4e6ff4ca0c482690cb32554e33e
Devihem <Devihem7@gmail.com>
2023-10-03 08:38:57 +0300

    Daily Tasks JOINS lab

--- subqueries_and_joins_lab/managers.sql
+++ subqueries_and_joins_lab/managers.sql
@@ -1,11 +1,13 @@
 SELECT
-	employee_id
-	,CONCAT(first_name, ' ', last_name) AS full_name
-	,department_id
-	,' ' AS department_name
+	e.employee_id
+	,CONCAT(e.first_name, ' ', e.last_name) AS full_name
+	,d.department_id
+	,d.name AS department_name
 
-FROM employees
+FROM employees AS e
+JOIN departments AS d
+ON e.employee_id = d.manager_id
 
 ORDER BY
-	employee_id
+	e.employee_id
 LIMIT 5
--- subqueries_and_joins_lab/towns_addresses.sql
+++ subqueries_and_joins_lab/towns_addresses.sql
@@ -9,6 +9,6 @@ JOIN
 ON
 	t.town_id = a.town_id
 WHERE
-	"name" IN ('Sofia','San Francisco','Carnation')
+	t."name" IN ('Sofia','San Francisco','Carnation')
 ORDER BY
 	t.town_id , a.address_id
